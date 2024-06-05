0e45504461361981224fc6cf30e381f18472ecae
김남우 <121301972+KNamWoo@users.noreply.github.com>
2024-06-05 09:15:19 +0900

    update

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+function getValueInText(){
+    let inputData = document.getElementById("address").value;
+    alert(inputData);
+}
+
 var container = document.getElementById('map');
 var options = {
     center: new kakao.maps.LatLng(33.450701, 126.570667),
