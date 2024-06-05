7496f65466bf1882c3070907a2632806bc542215
김남우 <121301972+KNamWoo@users.noreply.github.com>
2024-06-05 11:28:15 +0900

    update

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
+var infowindow = new kakao.infowindow({zIndex:1});
+
+var ps = new kakao.maps.services.Places();
+
 function search(){
     var input = document.getElementById('keyword').value;
-    alert(input);
     ps.keywordSearch(input, placesSearchCB);
+}
+
+function placesSearchCB(data, status, pagination){
+    if(status === kakao.maps.services.Status.OK){
+        var bounds = new kakao.maps.LatLngBounds();
+
+        for(var i=0; i<data.length; i++){
+            displayMarker(data[i]);
+            bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+        }
+
+        map.setBounds(bounds);
+    }
+}
+
+function displayMarker(place){
+    var marker = new kakao.maps.Marker({
+        map: map,
+        position: new kakao.maps.LatLng(place.y, place.x)
+    });
+
+    kakao.maps.event.addListener(marker, 'click', function(){
+        infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
+        infowindow.open(map, marker);
+    });
 }
