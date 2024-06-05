fa83b42837e869d7eda751cfc0d7e58e3527c264
김남우 <121301972+KNamWoo@users.noreply.github.com>
2024-06-05 11:23:31 +0900

    update

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-var container = document.getElementById('map');
-var options = {
-    center: new kakao.maps.LatLng(33.450701, 126.570667),
-    level: 10
-};
-
-var map = new kakao.maps.Map(container, options);
+function search(){
+    var input = document.getElementById('keyword').value;
+    alert(input);
+    ps.keywordSearch(input, placesSearchCB);
+}
