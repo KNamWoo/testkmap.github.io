79fe9b045cf0677400120a2377ce607623b45050
김남우 <121301972+KNamWoo@users.noreply.github.com>
2024-06-08 15:06:35 +0900

    update

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
+let search_arr = [];
+
 function placeSearchCB(data, status, pagination){
     if(status === kakao.maps.services.Status.OK){
-        let target = data;
+        let target = data[0];
         console.log(target);
+        const lat = target.y;
+        const lng = target.x;
+        const latlng = new naver.maps.LatLng(lat, lng);
+        marker = new naver.maps.Marker({
+            position: latlng,
+            map: map
+        })
+        if(search_arr.length == 0){
+            search_arr.push(marker);
+        }else{
+            search_arr.push(marker);
+            let pre_marker = search_arr.splice(0, 1);//이전 검색된 마커 추출
+            pre_marker[0].serMap(null);//이전 마커 지우기
+        }
+        map.serZoom(14, false);
+        map.panTo(latlng);
     }else{
         alert("결과 없음");
     }
